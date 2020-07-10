d0c6a99c43f71c94eb9000c4f891753fd3d6df8c
raul <raulhdz40@gmail.com>
2020-07-10 04:02:57 -0500

    dibujar con flechas o con mouse disponibles

--- eventos.js
+++ eventos.js
@@ -1,21 +1,29 @@
 var estado = 0;
 var modoDeDibujo = 0;
-var CLICKDERECHO = 0;
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39
+};
 
+var botonFlechas = document.getElementById("botoncitoFlechas");
+botonFlechas.addEventListener("click", dibujoFlechas);
 var botonLibre = document.getElementById("botoncitoLibre");
 botonLibre.addEventListener("click", dibujoLibre);
-var botonClick = document.getElementById("botoncitoClick");
-botonClick.addEventListener("click", dibujoClick);
-var botonMano = document.getElementById("botoncitoMano");
-botonMano.addEventListener("click", dibujoMano);
 
-document.addEventListener("touchmove", dibujarTeclado);
-//document.addEventListener("mousedown",presionarMouse);  
-//document.addEventListener("mouseup",soltarMouse);   
+document.addEventListener("keydown", dibujarTeclado);
+document.addEventListener("mousemove", dibujarTeclado);
+document.addEventListener("mousedown",presionarMouse);  
+document.addEventListener("mouseup",soltarMouse);   
 var cuadrito = document.getElementById("area_de_dibujo");
+var ancho = cuadrito.width;
+var alto = cuadrito.height;
 var papel = cuadrito.getContext("2d");
-var x = evento.offsetX;
-var y = evento.offsetY;
+var x = ancho/2;
+var y = alto/2;
+
+dibujarLinea("red", x-1, y-1, x+1, y+1, papel);
 
 //------------------------------------------------------------------------------------------------
 
@@ -31,9 +39,12 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo) {
 
 function dibujarTeclado(evento) {
     var colorcito = "green";
+    var movimiento = 10;
 
-    //MOUSE LIBRE Y MANO
+    //MOUSE LIBRE
     if (modoDeDibujo == 2) {
+        console.log("si se detectó el modo 2");
+
         if (estado == 1) {
             dibujarLinea(colorcito, x, y, evento.offsetX, evento.offsetY, papel);
             x = evento.offsetX;
@@ -42,11 +53,31 @@ function dibujarTeclado(evento) {
     }
 
     //MOUSE CLICK
-    else if (modoDeDibujo == 3 && estado == 1) {
-        if(evento.button == CLICKDERECHO) {
-            dibujarLinea(colorcito, x, y, evento.offsetX, evento.offsetY, papel);
-            x = evento.offsetX;
-            y = evento.offsetY;
+    else if (modoDeDibujo == 1) {
+        console.log("si se detectó el modo 1");
+        switch(evento.keyCode) {
+            case teclas.UP:
+                dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+                y = y - movimiento;
+            break
+    
+            case teclas.DOWN:
+                dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+                y = y + movimiento;
+            break
+    
+            case teclas.LEFT:
+                dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+                x = x - movimiento;
+            break
+    
+            case teclas.RIGHT:
+                dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+                x = x + movimiento;
+            break
+    
+            default:
+            break;
         }
     }
 
@@ -64,14 +95,12 @@ function soltarMouse(evento) {
     y = evento.offsetY;
 }
 
+function dibujoFlechas() {
+    modoDeDibujo = 1;
+    x = ancho/2;
+    y = alto/2;
+}
+
 function dibujoLibre() {
     modoDeDibujo = 2;
-}
-
-function dibujoMano() {
-    modoDeDibujo = 2;
-}
-
-function dibujoClick() {
-    modoDeDibujo = 3;
 }
